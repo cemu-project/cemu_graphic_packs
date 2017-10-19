2c2c6124a3d0725cef18d17066c492eefef00961
Michael <goldtextwitch@outlook.com>
2017-10-18 20:27:25 -0700

    Add Color Splash and Smash 4 to build script

--- build.sh
+++ build.sh
@@ -38,13 +38,14 @@ build_dir () {
 
 res_360p=( "Performance" 640 360 )
 res_540p=( "Performance" 960 540 )
+res_720p=( "Performance" 1280 720 )
 res_900p=( "Quality" 1600 900 )
 res_1080p=( "Quality" 1920 1080 )
+res_1080p219=( "Quality" 2560 1080 )
 res_1440p=( "Quality" 2560 1440 )
+res_1440p219=( "Quality" 3440 1440 )
 res_1800p=( "Quality" 3200 1800  )
 res_2160p=( "Quality" 3840 2160  )
-res_1080p219=( "Quality" 2560 1080 )
-res_1440p219=( "Quality" 3440 1440 )
 res_2160p219=( "Quality" 5120 2160 )
 res_2880p=( "Enthusiast" 5120 2880 )
 res_4320p=( "Enthusiast" 7680 4320 )
@@ -81,13 +82,14 @@ std_respack () {
 	done
 }
 
-res16by9=( "2880p" "4320p" "5760p" "360p" "540p" "1080p" "1440p" "1800p" "2160p" "900p" )
+res16by9=( "360p" "540p" "720p" "900p" "1080p" "1440p" "1800p" "2160p" "2880p" "4320p" "5760p" )
 res21by9=( "1080p219" "1440p219" "2160p219" )
 
 std_respack "Bayonetta" "${res16by9[@]}"
 std_respack "Bayonetta2" "${res16by9[@]}"
 std_respack "BreathOfTheWild" "${res16by9[@]}" "${res21by9[@]}"
 std_respack "CaptainToad" "${res16by9[@]}" "${res21by9[@]}"
+std_respack "ColorSplash" "${res16by9[@]}"
 std_respack "HyruleWarriors" "${res16by9[@]}" "${res21by9[@]}"
 std_respack "KirbyRainbowCurse" "${res16by9[@]}"
 std_respack "MarioKart8" "${res16by9[@]}" "${res21by9[@]}"
@@ -95,3 +97,4 @@ std_respack "MarioTennis" "${res16by9[@]}"
 std_respack "SonicLostWorld" "${res16by9[@]}" "${res21by9[@]}"
 std_respack "Splatoon" "${res16by9[@]}" "${res21by9[@]}"
 std_respack "SuperMario3DWorld" "${res16by9[@]}" "${res21by9[@]}"
+std_respack "SuperSmashBros" "${res16by9[@]}"
