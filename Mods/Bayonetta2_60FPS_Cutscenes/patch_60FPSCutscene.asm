1c6828fa8ceeccd81d02beb8cdf70c80061c9947
getdls <25286924+getdls@users.noreply.github.com>
2020-04-23 21:07:42 +0200

    Bayonetta 2 - 60 FPS Cutscenes

---
+++ Mods/Bayonetta2_60FPS_Cutscenes/patch_60FPSCutscene.asm
@@ -0,0 +1,20 @@
+[Bayo2USv0]#No patches released
+moduleMatches = 0xAF5D1A85
+.origin = codecave
+	
+_enable60:
+.int $enable60
+
+_cave60:
+lis r9, _enable60@ha
+lwz r9, _enable60@l(r9)
+cmpwi r9, 1
+bne skipTo30
+addi r0,r10,1
+blr
+skipTo30:
+xori r0,r10,2
+blr
+
+0x03351098 = bla _cave60
+#0x034461C4 = nop #Kill Vsync
