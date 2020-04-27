a12a597e02b857e4aa675601ee732d32ddd67c3e
getdls <25286924+getdls@users.noreply.github.com>
2020-04-27 22:37:10 +0200

    Bayonetta 2 - Improve high detail mod
    Now includes NPCs, and value is scalable  (for v5 packs)

--- Mods/Bayonetta2_HighLOD/patch_gameplayTweaks.asm
+++ Mods/Bayonetta2_HighLOD/patch_gameplayTweaks.asm
@@ -1,5 +1,7 @@
 [Bayo2USv0_GP]
 moduleMatches = 0xAF5D1A85
 .origin = codecave
-	
-0x1014F804 = .float 3.0
+
+#Value is scalable in v5  - 100 default, 200 twice the high LOD culling distance 
+0x100AA3A8 = .float 200.0 	
+#0x1014F804 = .float 3.0
