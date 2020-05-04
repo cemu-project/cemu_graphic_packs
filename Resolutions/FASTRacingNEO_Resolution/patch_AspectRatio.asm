e2255adfbfd5fc595b9581db602781bc0390a2f4
Crementif <26669564+Crementif@users.noreply.github.com>
2020-05-04 21:32:50 +0200

    [Fast Racing Neo] Add preliminary ultrawide support
    Did work on some ultrawide HUD stuff too, but patching doesn't really seem to be the answer here.

---
+++ Resolutions/FASTRacingNEO_Resolution/patch_AspectRatio.asm
@@ -0,0 +1,6 @@
+[AspectRatio_v64]
+moduleMatches = 0xfab25e0c
+
+0x1000AE0C = .float ($width/$height)/($gameWidth/$gameHeight)
+
+#0x1000008C = .float ($width/$height)/($gameWidth/$gameHeight) # Doesn't seem to be related to the HUD aspect ratio, but it's related.
