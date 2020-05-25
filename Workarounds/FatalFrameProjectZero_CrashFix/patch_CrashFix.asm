8fc95104741f8050a4319475ff46412719574e0e
Crementif <26669564+Crementif@users.noreply.github.com>
2020-05-25 17:13:07 +0200

    [Fatal Frame] Add 10nth drop crash workaround
    I want to thank chrissie for helping me test these! Works with all versions of the game thanks to him too.

---
+++ Workarounds/FatalFrameProjectZero_CrashFix/patch_CrashFix.asm
@@ -0,0 +1,16 @@
+[FatalFrame_US_V0] # US eshop version
+moduleMatches = 0xE93B31F6
+
+0x024A8F20 = nop
+
+
+[ProjectZero_EU_V0] # Various versions
+moduleMatches = 0xB54DBEB0, 0x5A9D6D31, 0x360FA109, 0x9D96F19B, 0x81551809, 0x77589D01, 0x2897C68C
+
+0x024A8908 = nop
+
+
+[Zero_JAP_V0] # Various versions
+moduleMatches = 0xA86AA5B2, 0x52992DCD, 0x60DC1C85 # Vanilla, Unknown Patch
+
+0x029aa2f8 = nop
