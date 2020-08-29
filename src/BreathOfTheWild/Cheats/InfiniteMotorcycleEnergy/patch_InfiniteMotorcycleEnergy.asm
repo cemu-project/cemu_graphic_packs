66b35fd93969b6ce8f523521eec0977c4739b454
Crementif <26669564+Crementif@users.noreply.github.com>
2020-08-29 03:09:42 +0200

    [BotW] Add V5 cheats
    Thanks to @MrPigglet for helping me with porting BotW and some other stuff. I've also "ported" these to Cemu patches (which will probably be our minimum Cemu version) and cleaned everything up.

---
+++ src/BreathOfTheWild/Cheats/InfiniteMotorcycleEnergy/patch_InfiniteMotorcycleEnergy.asm
@@ -0,0 +1,10 @@
+[BotW_InfiniteMotorcycleEnergy_V208]
+moduleMatches = 0x6267BFD0
+
+0x020957DC = fmr f12, f0
+
+
+[BotW_InfiniteMotorcycleEnergy_V176V192]
+moduleMatches = 0xFD091F9F,0xD472D8A5
+
+0x0209583C = fmr f12, f0
---
+++ src/BreathOfTheWild/Cheats/MotorcycleAllRegions/patch_MotorcycleAllRegions.asm
@@ -0,0 +1,4 @@
+[BotW_MotorcycleAllRegions_V208]
+moduleMatches = 0x6267BFD0
+
+0x02A32A30 = li r3, 1 ; Spawn motorcycle anywhere, credits to Zeikken (converting to patches) and leoetlino (finding the value).
