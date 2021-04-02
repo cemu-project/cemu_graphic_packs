21584dc87609a32e75c4617ad7db7682442490e5
M&M <goldtextwitch@outlook.com>
2021-04-01 18:12:00 -0700

    Mario has resurrected.

--- docs/assets/javascript/index.js
+++ docs/assets/javascript/index.js
@@ -19,6 +19,8 @@ const altNames = {
 	"hw": "Hyrule Warriors",
 	"mc": "Minecraft",
 	"slw": "Sonic Lost World",
+	"smm": "Super Mario Maker",
+	"mk8": "Mario Kart 8",
 	"tphd": "The Legend of Zelda: Twilight Princess HD",
 	"wwhd": "The Legend of Zelda: The Wind Waker HD",
 	// easter eggs
