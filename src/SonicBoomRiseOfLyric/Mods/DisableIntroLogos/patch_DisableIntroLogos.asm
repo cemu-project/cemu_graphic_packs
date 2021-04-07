616065a9634bdc74fc162dce9fa7bd37138feea7
Hyper <34012267+HyperBE32@users.noreply.github.com>
2021-04-07 21:36:19 +0100

    [Sonic Boom] Disable Intro Logos patch (#514)

---
+++ src/SonicBoomRiseOfLyric/Mods/DisableIntroLogos/patch_DisableIntroLogos.asm
@@ -0,0 +1,17 @@
+[WiiULauncher0]
+moduleMatches = 0x90DAC5CE
+
+; Jump to title from Japanese video
+0x34BA068 = b 0x34BA2C8
+
+; Jump to title from English video
+0x34BA074 = b 0x34BA2C8
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Jump to title from Japanese video
+0x34BA4B8 = b 0x34BA760
+
+; Jump to title from English video
+0x34BA4C4 = b 0x34BA760
