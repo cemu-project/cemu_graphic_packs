4361ca80fa7c55b7461d058857b97e604443dbee
HyperBE32 <HyperPolygon64@gmail.com>
2021-04-07 23:46:30 +0100

    [Sonic Boom] Added patch support for other revisions

--- src/SonicBoomRiseOfLyric/Mods/DisableIntroLogos/patch_DisableIntroLogos.asm
+++ src/SonicBoomRiseOfLyric/Mods/DisableIntroLogos/patch_DisableIntroLogos.asm
@@ -1,4 +1,4 @@
-[WiiULauncher0]
+[WiiULauncher0US]
 moduleMatches = 0x90DAC5CE
 
 ; Jump to title from Japanese video
@@ -6,6 +6,24 @@ moduleMatches = 0x90DAC5CE
 
 ; Jump to title from English video
 0x34BA074 = b 0x34BA2C8
+
+[WiiULauncher0EU]
+moduleMatches = 0x8F7D2702
+
+; Jump to title from Japanese video
+0x34BA0F0 = b 0x34BA350
+
+; Jump to title from English video
+0x34BA0FC = b 0x34BA350
+
+[WiiULauncher0JP]
+moduleMatches = 0x0D395735
+
+; Jump to title from Japanese video
+0x34BA430 = b 0x34BA690
+
+; Jump to title from English video
+0x34BA43C = b 0x34BA690
 
 [WiiULauncher16]
 moduleMatches = 0x113CC316
