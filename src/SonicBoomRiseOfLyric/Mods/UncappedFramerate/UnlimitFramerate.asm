d8b61cf93470a4be2bac50031d23afbb003a56b4
M&M <goldtextwitch@outlook.com>
2021-04-07 08:16:45 -0700

    Simplify Rise of Lyric uncap framerate patch

---
+++ src/SonicBoomRiseOfLyric/Mods/UncappedFramerate/UnlimitFramerate.asm
@@ -0,0 +1,11 @@
+[WiiULauncher0]
+moduleMatches = 0x90DAC5CE
+
+; Unconditionally branch DriverD3D::LimitFramerate immediately
+0x02E47110 = blr
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Unconditionally branch DriverD3D::LimitFramerate immediately
+0x02E47148 = blr
