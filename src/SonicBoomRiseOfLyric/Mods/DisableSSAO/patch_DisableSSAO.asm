107dbe6a2cbb8b0d2d2576d67087b1e92df0dca8
HyperBE32 <HyperPolygon64@gmail.com>
2021-04-09 03:03:35 +0100

    [Sonic Boom] Disable SSAO

---
+++ src/SonicBoomRiseOfLyric/Mods/DisableSSAO/patch_DisableSSAO.asm
@@ -0,0 +1,23 @@
+[WiiULauncher0US]
+moduleMatches = 0x90DAC5CE
+
+; Skip SSAO parameter call
+0x2E966AC = b 0x2E966E8
+
+[WiiULauncher0EU]
+moduleMatches = 0x8F7D2702
+
+; Skip SSAO parameter call
+0x2E9668C = b 0x2E966C8
+
+[WiiULauncher0JP]
+moduleMatches = 0x0D395735
+
+; Skip SSAO parameter call
+0x2E96448 = b 0x2E96484
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Skip SSAO parameter call
+0x2E966E4 = b 0x2E96720
