0070e6734a1b6faf5e14e20766a825c3a1fc5f0f
Hyper <34012267+HyperBE32@users.noreply.github.com>
2021-04-10 02:58:02 +0100

    [Sonic Boom] Fix Pause State Reset (#517)

---
+++ src/SonicBoomRiseOfLyric/Mods/FixPauseStateReset/patch_FixPauseStateReset.asm
@@ -0,0 +1,23 @@
+[WiiULauncher0US]
+moduleMatches = 0x90DAC5CE
+
+; Skip CBrbAICompanion::ForceIdle subroutine
+0x306EBA0 = nop
+
+[WiiULauncher0EU]
+moduleMatches = 0x8F7D2702
+
+; Skip CBrbAICompanion::ForceIdle subroutine
+0x306EB80 = nop
+
+[WiiULauncher0JP]
+moduleMatches = 0x0D395735
+
+; Skip CBrbAICompanion::ForceIdle subroutine
+0x306EA30 = nop
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Skip CBrbAICompanion::ForceIdle subroutine
+0x306EDBC = nop
