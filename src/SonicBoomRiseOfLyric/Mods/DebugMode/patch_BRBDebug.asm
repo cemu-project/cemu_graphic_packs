5395e51695f06007321806cfedb7321c8628fe42
M&M <goldtextwitch@outlook.com>
2021-04-12 21:27:48 -0700

    [Sonic Boom] Rename debug mode pack

--- src/SonicBoomRiseOfLyric/Mods/DebugMode/patch_BRBDebug.asm
+++ src/SonicBoomRiseOfLyric/Mods/DebugMode/patch_BRBDebug.asm
@@ -3,45 +3,45 @@ moduleMatches = 0x90DAC5CE
 
 ; Enable sys_brbdebug
 .origin = codecave
-DevLevelSelect:
+brbdebug:
 li r5, 1
 bctrl
 b 0x2C9B978
 
-0x2C9B974 = b DevLevelSelect
+0x2C9B974 = b brbdebug
 
 [WiiULauncher0EU]
 moduleMatches = 0x8F7D2702
 
 ; Enable sys_brbdebug
 .origin = codecave
-DevLevelSelect:
+brbdebug:
 li r5, 1
 bctrl
 b 0x2C9B958
 
-0x2C9B954 = b DevLevelSelect
+0x2C9B954 = b brbdebug
 
 [WiiULauncher0JP]
 moduleMatches = 0x0D395735
 
 ; Enable sys_brbdebug
 .origin = codecave
-DevLevelSelect:
+brbdebug:
 li r5, 1
 bctrl
 b 0x2C9B990
 
-0x2C9B98C = b DevLevelSelect
+0x2C9B98C = b brbdebug
 
 [WiiULauncher16]
 moduleMatches = 0x113CC316
 
 ; Enable sys_brbdebug
 .origin = codecave
-DevLevelSelect:
+brbdebug:
 li r5, 1
 bctrl
 b 0x2C9B9B0
 
-0x2C9B9AC = b DevLevelSelect
+0x2C9B9AC = b brbdebug
