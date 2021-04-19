9448a239967eddb2a5ff2522d97c2d0462f56043
M&M <goldtextwitch@outlook.com>
2021-04-19 16:00:23 -0700

    [Sonic Lost World] change speedrun effect patch and add issues note

--- src/SonicLostWorld/Mods/LinkSonicEverywhere/patch_Linksonic.asm
+++ src/SonicLostWorld/Mods/LinkSonicEverywhere/patch_Linksonic.asm
@@ -1,10 +1,10 @@
 [sonic2013_v48]
 moduleMatches = 0xC5691A40
 
-; load Linksonic.pac and chr_Linksonic.model
+; load SonicZeldaInfo on top of SonicInfo
 0x02A60414 = nop
 0x02A53680 = nop
 
-; load ef_dl3_linksonic_speedrun.effect
-0x02A43FD8 = nop
-0x02A44050 = nop
+; load ef_dl3_linksonic_speedrun instead of ef_ch_son_yh1_speedrun_loop
+0x02A43FE0 = nop
+0x02A44058 = nop
