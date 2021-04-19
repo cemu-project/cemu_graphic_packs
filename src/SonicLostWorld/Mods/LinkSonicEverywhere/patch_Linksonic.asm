57bc7e3652ce9b39c04fada1d0866c904cb2eb01
M&M <goldtextwitch@outlook.com>
2021-04-19 15:43:40 -0700

    [Sonic Lost World] Add "Link Sonic Everywhere" mod
    Co-authored-by: DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>

---
+++ src/SonicLostWorld/Mods/LinkSonicEverywhere/patch_Linksonic.asm
@@ -0,0 +1,10 @@
+[sonic2013_v48]
+moduleMatches = 0xC5691A40
+
+; load Linksonic.pac and chr_Linksonic.model
+0x02A60414 = nop
+0x02A53680 = nop
+
+; load ef_dl3_linksonic_speedrun.effect
+0x02A43FD8 = nop
+0x02A44050 = nop
