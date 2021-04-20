572b19adbcfd19e5fc8725b127248f0f09a501a3
HyperBE32 <HyperPolygon64@gmail.com>
2021-04-20 01:35:04 +0100

    [Sonic Boom] Disable Camera Shake

---
+++ src/SonicBoomRiseOfLyric/Enhancements/DisableEventCameraShake/patch_DisableEventCameraShake.asm
@@ -0,0 +1,23 @@
+[WiiULauncher0US]
+moduleMatches = 0x90DAC5CE
+
+; Skip shake initialisation in CView::ProcessShakeNormal
+0x2AEA96C = b 0x2AEAA0C
+
+[WiiULauncher0EU]
+moduleMatches = 0x8F7D2702
+
+; Skip shake initialisation in CView::ProcessShakeNormal
+0x2AEA94C = b 0x2AEA9EC
+
+[WiiULauncher0JP]
+moduleMatches = 0x0D395735
+
+; Skip shake initialisation in CView::ProcessShakeNormal
+0x2AEA988 = b 0x2AEAA28
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Skip shake initialisation in CView::ProcessShakeNormal
+0x2AEA9A8 = b 0x2AEAA48
---
+++ src/SonicBoomRiseOfLyric/Enhancements/DisablePlayerCameraShake/patch_DisablePlayerCameraShake.asm
@@ -0,0 +1,23 @@
+[WiiULauncher0US]
+moduleMatches = 0x90DAC5CE
+
+; Skip shake initialisation in CBrbGameCamera::InitializeCameraShakes
+0x309CA08 = b 0x309CBE8
+
+[WiiULauncher0EU]
+moduleMatches = 0x8F7D2702
+
+; Skip shake initialisation in CBrbGameCamera::InitializeCameraShakes
+0x309CA70 = b 0x309CC50
+
+[WiiULauncher0JP]
+moduleMatches = 0x0D395735
+
+; Skip shake initialisation in CBrbGameCamera::InitializeCameraShakes
+0x309CA28 = b 0x309CC08
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Skip shake initialisation in CBrbGameCamera::InitializeCameraShakes
+0x309CC28 = b 0x309CE08
