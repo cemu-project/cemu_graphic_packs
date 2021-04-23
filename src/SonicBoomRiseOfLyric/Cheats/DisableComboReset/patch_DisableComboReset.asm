145ef1dc8f5f5f1b1ec6d9c33ca0397bca6dff04
Hyper <34012267+HyperBE32@users.noreply.github.com>
2021-04-23 16:35:44 +0100

    [Sonic Boom] Disable Combo Reset (#524)

---
+++ src/SonicBoomRiseOfLyric/Cheats/DisableComboReset/patch_DisableComboReset.asm
@@ -0,0 +1,23 @@
+[WiiULauncher0US]
+moduleMatches = 0x90DAC5CE
+
+; Skip BRBCharacter::ResetCombo
+0x323C888 = blr
+
+[WiiULauncher0EU]
+moduleMatches = 0x8F7D2702
+
+; Skip BRBCharacter::ResetCombo
+0x323C920 = blr
+
+[WiiULauncher0JP]
+moduleMatches = 0x0D395735
+
+; Skip BRBCharacter::ResetCombo
+0x323CBB0 = blr
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Skip BRBCharacter::ResetCombo
+0x323CCA0 = blr
