74bc2928644e28e381bfa222e718a23b1e8b300a
intra <111546963+intra2@users.noreply.github.com>
2022-08-21 17:17:00 -0500

    Update patch_treasure.asm

--- src/XenobladeChroniclesX/Mods/LootTreasureQuality/patch_treasure.asm
+++ src/XenobladeChroniclesX/Mods/LootTreasureQuality/patch_treasure.asm
@@ -20,7 +20,7 @@ blr
 
 
 [XCX_LOOT_FORCECHEST_V101E_V102U_V100U]
-moduleMatches = 0xF882D5CF, 0x218F6E07, 0x30B6E091, 0xAB97DE6B, 0x676EB33E ; 1.0.1E, 1.0.0E, 1.0.2U, 1.0.1U, 1.0.0U
+moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07, 0xAB97DE6B, 0x676EB33E ; 1.0.1E, 1.0.2U, 1.0.0E, 1.0.1U, 1.0.0U
 0x021AAFA4 = bla _goForced
 ; ----------------------------------------------------------------------------
 ; WHO  : ItemDrop::CItemDropManager::calcRank((unsigned short, int))
