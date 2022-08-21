be1d8fc0076af50e34e4fd7f7c72caba798f0463
intra <111546963+intra2@users.noreply.github.com>
2022-08-21 17:15:03 -0500

    Update patch_camera.asm
    formatting again, want the order to be E,U,J(updated) E,U,J(non updated)

--- src/XenobladeChroniclesX/Mods/GraphicsCameraDistance/patch_camera.asm
+++ src/XenobladeChroniclesX/Mods/GraphicsCameraDistance/patch_camera.asm
@@ -6,7 +6,7 @@ VarZoom:
 ; CamData::CCamDataManager::getParamsZoomDefault((CamData::eTargetTypes, unsigned char))
 
 [XCX_CAMERA_DIST_V101E_V102U_V100U]
-moduleMatches = 0xF882D5CF, 0x30B6E091, 0xAB97DE6B, 0x218F6E07, 0x676EB33E ; 1.0.1E, 1.0.2U, 1.0.1U, 1.0.0E, 1.0.0U
+moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07, 0xAB97DE6B, 0x676EB33E ; 1.0.1E, 1.0.2U, 1.0.0E, 1.0.1U, 1.0.0U
 0x0222A19C = lis       r12, VarZoom@ha
 0x0222A1B0 = lfs       f1, VarZoom@l(r12)
 0x0222A1CC = nop
