d8409c276c26c07e7eab982d91aabcc0d1002504
intra <111546963+intra2@users.noreply.github.com>
2022-08-26 23:25:35 -0500

    Update patch_qte_soulvoices.asm

--- src/XenobladeChroniclesX/Mods/BattleQteSoulVoices/patch_qte_soulvoices.asm
+++ src/XenobladeChroniclesX/Mods/BattleQteSoulVoices/patch_qte_soulvoices.asm
@@ -1,4 +1,4 @@
-[XCX_QTE_AUTO_ALL_V101E_V102U_V100U]
+[XCX_QTE_AUTO_V101E_V102U_V100U]
 moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07, 0xAB97DE6B, 0x676EB33E ; 1.0.1E, 1.0.2U, 1.0.0E, 1.0.1U, 1.0.0U
 ; ----------------------------------------------------------------------------
 ; WHO  : Battle::CSoulVoiceButtonChallenge::applyResult((void))
@@ -11,14 +11,14 @@ moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07, 0xAB97DE6B, 0x676EB33E ; 1.0
 0x0209D6F0 = b _applyResult
 0x0209D720 = b _applyResult
 
-[XCX_QTE_AUTO_ALL_V102J]
+[XCX_QTE_AUTO_V102J]
 moduleMatches = 0x7672271D ; 1.0.2J
 0x0209B504 = li r9, 2
 0x0209D230 = _applyResult:
 0x0209D1BC = b _applyResult
 0x0209D1EC = b _applyResult
 
-[XCX_QTE_AUTO_ALL_V100J]
+[XCX_QTE_AUTO_V100J]
 moduleMatches = 0x785CA8A9 ; 1.0.0J
 0x0209B290 = li r9, 2
 0x0209CFBC = _applyResult:
