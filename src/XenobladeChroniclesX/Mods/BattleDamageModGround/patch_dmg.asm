d28f273d7c55255961ff7032e58b9995c2ed06b0
intra <111546963+intra2@users.noreply.github.com>
2022-08-29 10:39:07 -0500

    fixes damage mods
    made requested changes

--- src/XenobladeChroniclesX/Mods/BattleDamageModGround/patch_dmg.asm
+++ src/XenobladeChroniclesX/Mods/BattleDamageModGround/patch_dmg.asm
@@ -1,21 +1,22 @@
-; DO NOT add 1.0.2J support untill official XCX game servers go offline
-; DO NOT add 1.0.2U, 1.0.1E or 1.0.0E support untill official XCX game servers go offline
-;hi regwus, <3<3 intra <3<3
-
-[XCX_DAMAGES_V1] ; ####################################################################################################################################
-moduleMatches = 0x785CA8A9
-
+[XCX_DAMAGES]
+moduleMatches = 0xF882D5CF, 0x30B6E091, 0x7672271D, 0x218F6E07, 0xAB97DE6B, 0x676EB33E, 0x785CA8A9 ; 1.0.1E, 1.0.2U, 1.0.2J, 1.0.0E, 1.0.1U, 1.0.0U, 1.0.0J
 .origin = codecave
-
 .int $mult
 
-0x025CD268 = mulli r4, r31, $mult
+[XCX_DAMAGES_V101E_V102U]
+moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07 ; 1.0.1E, 1.0.2U, 1.0.0E
+0x02265BE4 = cmpw r0, r0
+0x025D896C = mulli r4, r31, $mult
 
-[XCX_DAMAGES_V2] ; ####################################################################################################################################
-moduleMatches = 0xAB97DE6B, 0x676EB33E
+;[XCX_DAMAGES_V102J]
+;DO NOT port to JP 1.0.2 untill official game servers go offline
 
-.origin = codecave
-
-.int $mult
-
+[XCX_DAMAGES_V100U]
+moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
+0x02265BE4 = cmpw r0, r0
 0x025D88FC = mulli r4, r31, $mult
+
+[XCX_DAMAGES_V100J]
+moduleMatches = 0x785CA8A9 ; 1.0.0J
+0x02265404 = cmpw r0, r0
+0x025CD268 = mulli r4, r31, $mult
--- src/XenobladeChroniclesX/Mods/BattleEnemyStats/patch_stats.asm
+++ src/XenobladeChroniclesX/Mods/BattleEnemyStats/patch_stats.asm
@@ -1,10 +1,5 @@
-; DO NOT add 1.0.2J support untill official XCX game servers go offline
-; DO NOT add 1.0.2U, 1.0.1E or 1.0.0E support untill official XCX game servers go offline
-;to remove the update go to your mlc folder then /usr/title/0005000e/ then change '101c4d00' to '-101c4d00' for the US version or change '10116100' to '-10116100' for the JP version. to install the updates again simply change the name(s) back to how they originally where
-
-[XCX_ENEMIESTATS]
-moduleMatches = 0xAB97DE6B, 0x676EB33E
-
+[XCX_ENEMIESTATS_V101E_V102U_V100U]
+moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07, 0xAB97DE6B, 0x676EB33E ; 1.0.1E, 1.0.2U, 1.0.0E, 1.0.1U, 1.0.0U
 ; ----------------------------------------------------------------------------
 ; WHO  : calcEnParam__3cfsFPQ2_3cfs7SChrPrmUiT2
 ; WHAT : Apply modificator on enemies stats (HP, Melee Attack, Melee Accuracy, Ranged Attack, Ranged Accuracy, Potential and Evasion)
@@ -13,11 +8,13 @@ moduleMatches = 0xAB97DE6B, 0x676EB33E
 ;  70% = 0.0007
 ;  50% = 0.0005
 ;   1% = 0.00001 ; GAME CRASH (enemis are too weak)
-
+0x02265BE4 = cmpw r0, r0
 0x1003206C = .float $mod
 
+;[XCX_ENEMIESTATS_V102J]
+;DO NOT port to JP 1.0.2 untill official game servers go offline
 
-
-[XCX_ENEMIESTATS_V]
-moduleMatches = 0x785CA8A9
+[XCX_ENEMIESTATS_V100J]
+moduleMatches = 0x785CA8A9 ; 1.0.0J
+0x02265404 = cmpw r0, r0
 0x10031FDC = .float $mod
