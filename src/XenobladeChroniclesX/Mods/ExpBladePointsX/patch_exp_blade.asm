ff1c35c90a91a50f784bf5836000c240b6d00be3
intra <111546963+intra2@users.noreply.github.com>
2022-09-13 11:49:10 -0500

    Add files via upload

--- src/XenobladeChroniclesX/Mods/ExpBladePointsX/patch_exp_blade.asm
+++ src/XenobladeChroniclesX/Mods/ExpBladePointsX/patch_exp_blade.asm
@@ -8,13 +8,13 @@ moduleMatches = 0xF882D5CF, 0x30B6E091, 0x7672271D, 0x218F6E07, 0xAB97DE6B, 0x67
 
 [XCX_BLADEX_V101E]
 moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
-0x0288E610 = nop
+0x0288E610 = nop ;
 0x0295B678 = li r3, -1
 0x0288E614 = li r3, $mod
 
 [XCX_BLADEX_V102U]
 moduleMatches = 0x30B6E091 ; 1.0.2U
-0x0288E610 = nop
+0x0288E610 = nop ;
 0x0295B668 = li r3, -1
 0x0288E614 = li r3, $mod
 
@@ -26,10 +26,10 @@ moduleMatches = 0x30B6E091 ; 1.0.2U
 
 [XCX_BLADEX_V100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
-0x0288E594 = nop
+0x0288E594 = nop ;
 0x0295B5EC = li r3, -1
 0x0288E598 = li r3, $mod
 
 ;[XCX_BLADEX_V100J]
-;moduleMatches = 0x785CA8A9 ; 1.0.0J
+;moduleMatches = 0x785CA8A9 ; 1.0.0J ;;;
 ;getUnionBasePoint__Q2_2fw11SocialUnionFi does not exist
