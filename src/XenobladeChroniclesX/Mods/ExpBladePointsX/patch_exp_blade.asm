b387ed4b90c12fbe1f3174c58d2c7d44c047a2f9
intra <111546963+intra2@users.noreply.github.com>
2022-09-13 10:21:02 -0500

    Add files via upload
    equipment best quality got a feature update (tested) and randomAffix also got a feature update (kinda tested kinda untested), it works, but it would take a few hours of grinding to discover if I got the desired ratio or not

--- src/XenobladeChroniclesX/Mods/ExpBladePointsX/patch_exp_blade.asm
+++ src/XenobladeChroniclesX/Mods/ExpBladePointsX/patch_exp_blade.asm
@@ -6,19 +6,28 @@ moduleMatches = 0xF882D5CF, 0x30B6E091, 0x7672271D, 0x218F6E07, 0xAB97DE6B, 0x67
 ; WHO  : fw::SocialUnion::getUnionBasePoint((int))
 ; WHAT : Multiply the BLADE points rewarded
 
-[XCX_BLADEX_V101E_V102U]
-moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07 ; 1.0.1E, 1.0.2U, 1.0.0E
+[XCX_BLADEX_V101E]
+moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
 0x0288E610 = nop
+0x0295B678 = li r3, -1
 0x0288E614 = li r3, $mod
 
-[XCX_BLADEX_V102J]
-moduleMatches = 0x7672271D ; 1.0.2J
-0x0288B470 = nop
-0x0288B474 = li r3, $mod
+[XCX_BLADEX_V102U]
+moduleMatches = 0x30B6E091 ; 1.0.2U
+0x0288E610 = nop
+0x0295B668 = li r3, -1
+0x0288E614 = li r3, $mod
+
+;[XCX_BLADEX_V102J]
+;moduleMatches = 0x7672271D ; 1.0.2J
+;0x = nop ; broken
+;0x = li r3, -1 ; broken
+;0x = li r3, $mod ; broken
 
 [XCX_BLADEX_V100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
 0x0288E594 = nop
+0x0295B5EC = li r3, -1
 0x0288E598 = li r3, $mod
 
 ;[XCX_BLADEX_V100J]
--- src/XenobladeChroniclesX/Mods/LootEquipmentsBestQuality/patch_quality.asm
+++ src/XenobladeChroniclesX/Mods/LootEquipmentsBestQuality/patch_quality.asm
@@ -2,7 +2,7 @@
 moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07, 0xAB97DE6B, 0x676EB33E ; 1.0.1E, 1.0.2U, 1.0.0E, 1.0.1U, 1.0.0U
 ; ----------------------------------------------------------------------------
 ; WHO  : __CPR98__calcItemTableID__Q2_8ItemDrop16CItemDropManagerFUcRCQ3_J20JJ29J11SCreateInfoRUs
-; WHAT : Always best equipment looted (X6X)
+; WHAT : Always best equipment looted (XCX)
 0x021AD710 = nop ; ground weapons ?
 0x021ADA6C = nop ; ground armors
 0x021ADDE8 = nop ; skell weapons
@@ -10,8 +10,8 @@ moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07, 0xAB97DE6B, 0x676EB33E ; 1.0
 ; ----------------------------------------------------------------------------
 ; WHO  : ItemDrop::CItemDropManager::calcAfxSltNum((unsigned short))
 ; WHAT : Control of the loot quality (always orange/yellow)
-0x021AEE7C = li r3, 3
-0x021AF280 = li r3, 3
+0x021AEE7C = li r3, $traits ; num of gear traits
+0x021AF280 = li r3, $slots ; num of gear slots
 
 [XCX_LOOT_EQUIPMENT_BESTQUALITY_V102J]
 moduleMatches = 0x7672271D ; 1.0.2J
@@ -20,8 +20,8 @@ moduleMatches = 0x7672271D ; 1.0.2J
 0x021AD8B4 = nop ; skell weapons
 0x021ADC30 = nop ; skell armors
 
-0x021AE948 = li r3, 3
-0x021AED4C = li r3, 3
+0x021AE948 = li r3, $traits ; num of gear traits
+0x021AED4C = li r3, $slots ; num of gear slots
 
 [XCX_LOOT_EQUIPMENT_BESTQUALITY_V100J]
 moduleMatches = 0x785CA8A9 ; 1.0.0J
@@ -30,5 +30,5 @@ moduleMatches = 0x785CA8A9 ; 1.0.0J
 0x021AD630 = nop ; skell weapons
 0x021AD9AC = nop ; skell armors
 
-0x021AE6C4 = li r3, 3
-0x021AEAC8 = li r3, 3
+0x021AE6C4 = li r3, $traits ; num of gear traits
+0x021AEAC8 = li r3, $slots ; num of gear slots
