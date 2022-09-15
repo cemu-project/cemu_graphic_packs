5a89d7612889812a62b7c83c57d92971c58f4f6f
intra <111546963+intra2@users.noreply.github.com>
2022-09-15 17:59:20 -0500

    Add files via upload

--- src/XenobladeChroniclesX/Mods/ExpBladePointsX/patch_exp_blade.asm
+++ src/XenobladeChroniclesX/Mods/ExpBladePointsX/patch_exp_blade.asm
@@ -9,13 +9,13 @@ moduleMatches = 0xF882D5CF, 0x30B6E091, 0x7672271D, 0x218F6E07, 0xAB97DE6B, 0x67
 [XCX_BLADEX_V101E]
 moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
 0x0288E610 = nop
-0x0295B678 = li r3, -1
+0x02E0C5B0 = li r3, -1
 0x0288E614 = li r3, $mod
 
 [XCX_BLADEX_V102U]
 moduleMatches = 0x30B6E091 ; 1.0.2U
 0x0288E610 = nop
-0x0295B668 = li r3, -1
+0x02E0C550 = li r3, -1
 0x0288E614 = li r3, $mod
 
 ;[XCX_BLADEX_V102J]
@@ -27,7 +27,7 @@ moduleMatches = 0x30B6E091 ; 1.0.2U
 [XCX_BLADEX_V100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
 0x0288E594 = nop
-0x0295B5EC = li r3, -1
+0x02E0C3D8 = li r3, -1
 0x0288E598 = li r3, $mod
 
 ;[XCX_BLADEX_V100J]
