eaacceb86fb6a06d9dfb2adb989d6f15d9562952
intra <111546963+intra2@users.noreply.github.com>
2022-09-15 18:00:24 -0500

    Add files via upload

--- src/XenobladeChroniclesX/Mods/BattleDamageModGround2/patch_dmg.asm
+++ src/XenobladeChroniclesX/Mods/BattleDamageModGround2/patch_dmg.asm
@@ -10,12 +10,12 @@ blr
 
 [XCX_DAMAGES_V101E]
 moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
-0x0295B678 = li r3, -1-$divr
+0x02E0C5B0 = li r3, -1-$divr
 0x025D896C = bla _divide_dmg
 
 [XCX_DAMAGES_V102U]
 moduleMatches = 0x30B6E091 ; 1.0.2U
-0x0295B668 = li r3, -1-$divr
+0x02E0C550 = li r3, -1-$divr
 0x025D896C = bla _divide_dmg
 
 ;[XCX_DAMAGES_V102J]
@@ -23,10 +23,10 @@ moduleMatches = 0x30B6E091 ; 1.0.2U
 
 [XCX_DAMAGES_V100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
-0x0295B5EC = li r3, -1-$divr
+0x02E0C3D8 = li r3, -1-$divr
 0x025D88FC = bla _divide_dmg
 
 [XCX_DAMAGES_V100J]
 moduleMatches = 0x785CA8A9 ; 1.0.0J
-0x02956D10 = li r3, -1-$divr
+0x02E03228 = li r3, -1-$divr
 0x025CD268 = bla _divide_dmg
--- src/XenobladeChroniclesX/Mods/BattleEnemyStats/patch_stats.asm
+++ src/XenobladeChroniclesX/Mods/BattleEnemyStats/patch_stats.asm
@@ -8,12 +8,12 @@ moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
 ;  70% = 0.0007
 ;  50% = 0.0005
 ;   1% = 0.00001 ; GAME CRASH (enemis are too weak)
-0x0295B678 = li r3, -3
+0x02E0C5B0 = li r3, -3
 0x1003206C = .float $mod
 
 [XCX_ENEMIESTATS_V102U]
 moduleMatches = 0x30B6E091 ; 1.0.2U
-0x0295B668 = li r3, -3
+0x02E0C550 = li r3, -3
 0x1003206C = .float $mod
 
 ;[XCX_ENEMIESTATS_V102J]
@@ -21,10 +21,10 @@ moduleMatches = 0x30B6E091 ; 1.0.2U
 
 [XCX_ENEMIESTATS_V100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
-0x0295B5EC = li r3, -3
+0x02E0C3D8 = li r3, -3
 0x1003206C = .float $mod
 
 [XCX_ENEMIESTATS_V100J]
 moduleMatches = 0x785CA8A9 ; 1.0.0J
-0x02956D10 = li r3, -3
+0x02E03228 = li r3, -3
 0x10031FDC = .float $mod
