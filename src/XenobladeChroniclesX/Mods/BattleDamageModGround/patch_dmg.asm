2937a3f1cd6cbae4d37bbd3559e8932fe879e9a1
intra <111546963+intra2@users.noreply.github.com>
2022-09-25 01:10:48 -0500

    Update src/XenobladeChroniclesX/Mods/BattleDamageModGround/patch_dmg.asm
    Co-authored-by: Crementif <26669564+Crementif@users.noreply.github.com>

--- src/XenobladeChroniclesX/Mods/BattleDamageModGround/patch_dmg.asm
+++ src/XenobladeChroniclesX/Mods/BattleDamageModGround/patch_dmg.asm
@@ -10,7 +10,6 @@ moduleMatches = 0x30B6E091 ; 1.0.2U
 
 ;[XCX_DAMAGES_V102J]
 ;DO NOT port to JP 1.0.2 untill official game servers go offline
-;hello hernan/renfaws/regwus <3<3
 
 [XCX_DAMAGES_V100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
