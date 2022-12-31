3771e7353173bac4f1b6b58e863743472d71be81
intra <111546963+intra2@users.noreply.github.com>
2022-12-31 02:17:28 -0600

    Add files via upload
    fix for blade metal mod

--- src/XenobladeChroniclesX/Mods/BladeMetals/patch_Metal.asm
+++ src/XenobladeChroniclesX/Mods/BladeMetals/patch_Metal.asm
@@ -1,19 +1,29 @@
-[XCX_BladeMetal_V102U]
-moduleMatches = 0x30B6E091 ; 1.0.2U
-0x02CFD54C = li r3, $metals
-
 [XCX_BladeMetal_V101E]
 moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
-0x02CFD5AC = li r3, $metals
+0x02888DF8 = li r3, $metals
+0x02888DE8 = li r3, $metals
+0x02888E04 = nop ; removes 9999 blade metal cap, loops to negatives at 32767
+
+[XCX_BladeMetal_V102U]
+moduleMatches = 0x30B6E091 ; 1.0.2U
+0x02888DF8 = li r3, $metals
+0x02888DE8 = li r3, $metals
+0x02888E04 = nop
 
 [XCX_BladeMetal_V102J]
 moduleMatches = 0x7672271D ; 1.0.2J
-0x02CF8A9C = li r3, $metals
+0x02885D58 = li r3, $metals
+0x02885D48 = li r3, $metals
+0x02885D64 = nop
 
 [XCX_BladeMetal_V100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
-0x02CFD3D4 = li r3, $metals
+0x02888D7C = li r3, $metals
+0x02888D6C = li r3, $metals
+0x02888D88 = nop
 
 [XCX_BladeMetal_V100J]
 moduleMatches = 0x785CA8A9 ; 1.0.0J
-0x02CF47A8 = li r3, $metals
+0x02884980 = li r3, $metals
+0x02884970 = li r3, $metals
+0x0288498C = nop
