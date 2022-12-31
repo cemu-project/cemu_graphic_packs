0373bb4aca1ce2e91ea58c7207d98ff6efe07d61
intra <111546963+intra2@users.noreply.github.com>
2022-12-30 23:41:48 -0600

    Add files via upload

---
+++ src/XenobladeChroniclesX/Mods/BladeMetals/patch_Metal.asm
@@ -0,0 +1,19 @@
+[XCX_BladeMetal_V102U]
+moduleMatches = 0x30B6E091 ; 1.0.2U
+0x02CFD54C = li r3, $metals
+
+[XCX_BladeMetal_V101E]
+moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
+0x02CFD5AC = li r3, $metals
+
+[XCX_BladeMetal_V102J]
+moduleMatches = 0x7672271D ; 1.0.2J
+0x02CF8A9C = li r3, $metals
+
+[XCX_BladeMetal_V100U]
+moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
+0x02CFD3D4 = li r3, $metals
+
+[XCX_BladeMetal_V100J]
+moduleMatches = 0x785CA8A9 ; 1.0.0J
+0x02CF47A8 = li r3, $metals
