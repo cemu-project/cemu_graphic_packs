cee416cfba54e06f615837a68dd1601cafb0d8db
intra0 <videogamesbecool@gmail.com>
2024-12-13 18:36:16 -0600

    Update MK8 mods
    adds v1 support to 60fps and resolution pack
    adds new course unlock pack and vehicle unlock pack

--- src/MarioKart8/Graphics/patch_LevelOfDetail.asm
+++ src/MarioKart8/Graphics/patch_LevelOfDetail.asm
@@ -1,4 +1,7 @@
-[MK8_LevelOfDetail_V42_V41]
-moduleMatches = 0x9F0A90B7,0xD09700CE
-
+[MarioKart8_LevelOfDetail_v42_v41]
+moduleMatches = 0x9F0A90B7, 0xD09700CE ; v4.2(EU/NA/JP), v4.1(EU/NA/JP)
 0x10121DF0 = .float $levelOfDetail
+
+[MarioKart8_LevelOfDetail_v1]
+moduleMatches = 0x2A2DC82C ; v1.0(EU/NA/JP)
+0x100F33F0 = .float $levelOfDetail
--- src/MarioKart8/NetworkPort/patch_port.asm
+++ src/MarioKart8/NetworkPort/patch_port.asm
@@ -1,7 +1,6 @@
-[MarioKart8_ForceMultiplayerPortNumber_v81]
-moduleMatches = 0x9F0A90B7
-
-# min port range
+[MarioKart8_ForceMultiplayerPortNumber_v42]
+moduleMatches = 0x9F0A90B7 ; v4.2(EU/NA/JP)
+; min port range
 0x101a9a52 = .short $port
-# max port range
+; max port range
 0x101a9a54 = .short $port
