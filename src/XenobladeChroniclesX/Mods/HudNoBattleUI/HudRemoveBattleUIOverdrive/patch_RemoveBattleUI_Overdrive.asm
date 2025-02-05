6d345551aae986fbac4f74bc2731c10953a6da6c
Intra <videogamesbecool@gmail.com>
2025-02-05 14:41:21 -0600

    New UI removal mods
    still more clean up needed for these but publishing whats finished for now

---
+++ src/XenobladeChroniclesX/Mods/HudNoBattleUI/HudRemoveBattleUIOverdrive/patch_RemoveBattleUI_Overdrive.asm
@@ -0,0 +1,28 @@
+[XCX_RemoveBattleUI_Overdrive_v101E]
+moduleMatches = 0xF882D5CF, 0x218F6E07 ; 1.0.1E, 1.0.0E
+;MISSING: overdrive start animation
+;debating wether to sperate the OD bonus & count into their own packs but keeping them in 1 for now
+;no ui remove overdrive bonus
+0x021bee34 = nop ;gearStart__8MenuTaskFUiUcT2
+;no ui remove overdrive count
+0x02c77fc4 = blr ;gearAddCount__8MenuTaskFUiUc
+
+[XCX_RemoveBattleUI_Overdrive_v102U]
+moduleMatches = 0x30B6E091 ; 1.0.2U
+0x021bee34 = nop ;gearStart__8MenuTaskFUiUcT2
+0x02c77f64 = blr ;gearAddCount__8MenuTaskFUiUc
+
+[XCX_RemoveBattleUI_Overdrive_v102J]
+moduleMatches = 0x7672271D ; 1.0.2J
+0x021be900 = nop ;gearStart__8MenuTaskFUiUcT2
+0x02c734b8 = blr ;gearAddCount__8MenuTaskFUiUc
+
+[XCX_RemoveBattleUI_Overdrive_v100U]
+moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
+0x021bee34 = nop ;gearStart__8MenuTaskFUiUcT2
+0x02c77dec = blr ;gearAddCount__8MenuTaskFUiUc
+
+[XCX_RemoveBattleUI_Overdrive_v100J]
+moduleMatches = 0x785CA8A9 ; 1.0.0J
+0x021be67c = nop ;gearStart__8MenuTaskFUiUcT2
+0x02c6f9a4 = blr ;gearAddCount__8MenuTaskFUiUc
