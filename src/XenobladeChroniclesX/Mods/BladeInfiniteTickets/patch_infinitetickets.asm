88e463c0590c6fbd9c7ee244ae384c31829f6aa5
Intra <videogamesbecool@gmail.com>
2025-02-12 22:49:18 -0600

    mix mismatchedname

--- src/XenobladeChroniclesX/Mods/BladeInfiniteTickets/patch_infinitetickets.asm
+++ src/XenobladeChroniclesX/Mods/BladeInfiniteTickets/patch_infinitetickets.asm
@@ -6,12 +6,12 @@ ticketammount = $ticketammount
 miraniumammount = $miraniumammount
 moneyammount = $moneyammount
 
-InfinitTickets:
+InfiniteTickets:
 lis r3, ticketammount@hi
 ori r3, r3, ticketammount@l
 blr
 
-InfinitMiranium:
+InfiniteMiranium:
 lis r3, miraniumammount@hi
 ori r3, r3, miraniumammount@l
 blr
@@ -29,17 +29,18 @@ moduleMatches = 0xF882D5CF, 0x30B6E091, 0x218F6E07 ; 1.0.1E, 1.0.2U, 1.0.0E
 
 [XCX_InfiniteTickets_v102J]
 moduleMatches = 0x7672271D ; 1.0.2J
-0x027F484C = ba InfinitTickets
+0x027F46F4 = ba InfiniteMoney
+0x027F484C = ba InfiniteTickets
 0x027F48E8 = ba InfinitMiranium
 
 [XCX_InfiniteTickets_v100U]
 moduleMatches = 0xAB97DE6B, 0x676EB33E ; 1.0.1U, 1.0.0U
 0x027F6AD4 = ba InfiniteMoney
-0x027F6C2C = ba InfinitTickets
-0x027F6CC8 = ba InfinitMiranium
+0x027F6C2C = ba InfiniteTickets
+0x027F6CC8 = ba InfiniteMiranium
 
 [XCX_InfiniteTickets_v100J]
 moduleMatches = 0x785CA8A9 ; 1.0.0J
 0x027F36C8 = ba InfiniteMoney
-0x027F3820 = ba InfinitTickets
-0x027F38BC = ba InfinitMiranium
+0x027F3820 = ba InfiniteTickets
+0x027F38BC = ba InfiniteMiranium
