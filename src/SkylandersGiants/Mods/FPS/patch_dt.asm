a897cf2a9a06d5683a760eda949e4844f3de8a31
Martino Fontana <tinozzo123@gmail.com>
2025-02-24 19:20:45 +0100

    [Skylanders] Fix speed-up above 120 FPS (SSA, SG and STT)

---
+++ src/SkylandersGiants/Mods/FPS/patch_dt.asm
@@ -0,0 +1,4 @@
+[SG_dt]
+moduleMatches = 0x62CC63A5
+
+0x02110EF4 = nop
---
+++ src/SkylandersTrapTeam/Mods/FPS/patch_dt.asm
@@ -0,0 +1,9 @@
+[STT_dt_V16]
+moduleMatches = 0x2A14BB42
+
+0x0210116C = b .+0xC
+
+[STT_dt_V1]
+moduleMatches = 0x321D97F0
+
+0x021016D8 = b .+0xC
