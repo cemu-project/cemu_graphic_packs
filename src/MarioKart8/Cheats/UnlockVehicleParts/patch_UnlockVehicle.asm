f7de3e8b2c62187c73b4c998c315f223349f945d
Intra <videogamesbecool@gmail.com>
2025-04-14 00:52:01 -0500

    completely record vehicle parts cheat
    the old cheat worked by updating the coin count in your play stats
    this new version works by updating the vehicle unlock flags directly.
    
    advantages being: doesn't modify your play stats, you no longer need to get a whole bunch of "new vehicle unlocked prompts", can unlock golden tiers and golden kart

--- src/MarioKart8/Cheats/UnlockVehicleParts/patch_UnlockVehicle.asm
+++ src/MarioKart8/Cheats/UnlockVehicleParts/patch_UnlockVehicle.asm
@@ -1,21 +1,57 @@
-[MarioKart8_UnlockVehicle]
+[MarioKart8_UnlockVehicleParts]
 moduleMatches = 0x9F0A90B7, 0xD09700CE ; v4.2(EU/NA/JP), v4.1(EU/NA/JP)
 .origin = codecave
 
-CheckandChangeCoinAmmount:
-lwz r0, 0x14e0(r26)
-cmpwi r0, $coinammount
-bge CheckandChangeCoinAmmount_Exit
-li r0, $coinammount
-stw r0, 0x14e0(r26)
-CheckandChangeCoinAmmount_Exit:
+UnlockVehicleParts:
+;r28 = 0x2e6b66D0
+li r3, 3
+
+;Kart Frames
+lbz r0, 0xD(r28)
+li r12, 26
+mtctr r12
+addi r12, r28, -1
+_forloopWritePartsFrames:
+ stbu r3, 1(r12)
+ bdnz _forloopWritePartsFrames
+stb r0, 0xD(r28)
+
+;Tires
+lbz r0, 0x40+0x11(r28)
+li r12, 18
+mtctr r12
+addi r12, r28, 0x40-1
+_forloopWritePartsTires:
+ stbu r3, 1(r12)
+ bdnz _forloopWritePartsTires
+stb r0, 0x40+0x11(r28)
+
+;Gliders
+lbz r0, 0x80+0xB(r28)
+li r12, 12
+mtctr r12
+addi r12, r28, 0x80-1
+_forloopWritePartsGliders:
+ stbu r3, 1(r12)
+ bdnz _forloopWritePartsGliders
+stb r0, 0x80+0xB(r28)
+
+;check if gold parts cheat is enabled
+li r0, 0
+cmpwi r0, $goldpartscheat
+beq UnlockVehicleParts_Exit
+stb r3, 0xD(r28)
+stb r3, 0x40+0x11(r28)
+stb r3, 0x80+0xB(r28)
+
+UnlockVehicleParts_Exit:
+lbzx r12, r28, r30
 blr
 
-[MarioKart8_UnlockVehicle_v42]
+[MarioKart8_UnlockVehicleParts_v42]
 moduleMatches = 0x9F0A90B7 ; v4.2(EU/NA/JP)
-;this mod makes 0x2e6b6128 = $coinammount
-0x025A4038 = bla CheckandChangeCoinAmmount
+0x024DB044 = bla UnlockVehicleParts
 
-[MarioKart8_UnlockVehicle_v41]
+[MarioKart8_UnlockVehicleParts_v41]
 moduleMatches = 0xD09700CE ; v4.1(EU/NA/JP)
-0x025A3E84 = bla CheckandChangeCoinAmmount
+0x024DACC0 = bla UnlockVehicleParts
