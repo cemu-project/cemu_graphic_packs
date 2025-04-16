0b6573049ce0db7f69c7a5c37d56f865c986e577
WinnerNombre <64211853+WinnerNombre@users.noreply.github.com>
2025-04-16 22:28:57 +0200

    [Skylanders] Remove Portal Check SWAP-Force

---
+++ src/SkylandersSwapForce/Mods/Remove Portal Check/patch_RemovePortalCheck.asm
@@ -0,0 +1,9 @@
+[SSF_RemovePortalCheck_V16]
+moduleMatches = 0xa0b35374 ; v16
+
+0x024B0A28 = li r3, 0x203
+
+[SSF_RemovePortalCheck_V0]
+moduleMatches = 0xb1f102ec ; v0
+
+0x024B09A0 = li r3, 0x203
