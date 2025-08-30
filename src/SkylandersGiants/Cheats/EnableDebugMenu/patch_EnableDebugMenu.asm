5504e316595a7457d2d87a810451cff0f764a8ba
Crementif <26669564+Crementif@users.noreply.github.com>
2025-08-31 01:38:02 +0200

    Tweak location and description of graphic pack

--- src/SkylandersGiants/Cheats/EnableDebugMenu/patch_EnableDebugMenu.asm
+++ src/SkylandersGiants/Cheats/EnableDebugMenu/patch_EnableDebugMenu.asm
@@ -1,4 +1,4 @@
-[SG_EnableDebug]
+[SG_EnableDebugMenu]
 moduleMatches = 0x62CC63A5, 0x71E3CF7E ; EUR, USA
 
 0x02004E08 = li r6, 1 ; changes the value of param_4 in this call of tfbCore::tfbStreamMemoryConfiguration::getPlatformSetup to 1 to load the debug folder
