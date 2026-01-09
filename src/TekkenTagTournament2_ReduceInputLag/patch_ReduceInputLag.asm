f816e8edd46a9792693ff2a0be66f4457fe68970
WAZAAAAA <WAZAAAAA0@users.noreply.github.com>
2026-01-09 04:25:51 +0100

    [TTT2] Add "Reduce input lag" mod
    free 2F input latency reduction

---
+++ src/TekkenTagTournament2_ReduceInputLag/patch_ReduceInputLag.asm
@@ -0,0 +1,9 @@
+[ReduceInputLag1]
+moduleMatches = 0x6527BCF5 #USAv16/EURv16/JPNv0
+0x2EF885C = li r7, 0
+
+[ReduceInputLag2]
+moduleMatches = 0x465EA719 #USAv32/EURv32/JPNv16
+0x2EF6EDC = li r7, 0
+
+#OFF/5/4/3/2/1bars=2/3/5/8/10/15frames - AOB:900A001080EB0000->900A001038E00000 - the known affected Bandai Namco fighting games are TTT2/SC5=2F TK7/SC6=1F
