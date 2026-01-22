275800dae2ffd456511692e252f2ecb8ef736cd3
WinnerNombre <64211853+WinnerNombre@users.noreply.github.com>
2026-01-22 23:33:00 +0100

    [Skylanders] Minor reorganization of "Enable Debug Menu" packs (#735)

--- src/SkylandersGiants/Cheats/EnableDebugMenu/patch_EnableDebugMenu.asm
+++ src/SkylandersGiants/Cheats/EnableDebugMenu/patch_EnableDebugMenu.asm
@@ -2,4 +2,4 @@
 moduleMatches = 0x62CC63A5, 0x71E3CF7E ; EUR, USA
 
 0x02004E08 = li r6, 1 ; changes the value of param_4 in this call of tfbCore::tfbStreamMemoryConfiguration::getPlatformSetup to 1 to load the debug folder
-0x02110098 = li r8, $app_config_value ; changes the App Config
+0x02110098 = li r8, $game_config_value ; changes the Game Configuration
